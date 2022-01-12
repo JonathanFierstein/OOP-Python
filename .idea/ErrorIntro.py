a80422c615fbda23dc389b05e7af42a4c65139b5
Jonathan Fierstein <48401556+jonathanfierstein@users.noreply.github.com>
2022-01-12 02:34:50 -0800

    ErrorIntro.py was created as a file for using the material learned in #5 Intro to errors.   I watched it for about 5 minutes, but there are still 7 minutes remaining in this lecture.
    Signed-off-by: Jonathan Fierstein <48401556+JonathanFierstein@users.noreply.github.com>

---
+++ .idea/ErrorIntro.py
@@ -0,0 +1,10 @@
+# #5 - An Intro to Errors in Python
+
+my_variable = "Read your traceback to solve your errors"
+print(my_variable)  #gives a name error because my_variable has not been defined
+
+my_variabletwo = 12.77865
+print(my_variabletwo)
+
+### I stopped this lecture with 7minutes remaining.   I should restart from this point...
+
